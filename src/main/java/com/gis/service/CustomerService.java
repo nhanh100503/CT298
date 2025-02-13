e67683bfb5986064154a0657ea5b160aa5c5ecc0
NGUYEN_HOANG_ANH <nhanh100503@gmail.com>
2025-02-13 13:10:56 +0700

    Update BookingService

---
+++ src/main/java/com/gis/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.gis.service;
+
+import com.gis.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+}
