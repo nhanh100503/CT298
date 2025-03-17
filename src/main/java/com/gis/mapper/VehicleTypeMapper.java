5a780cb5beddeaf40545901806206adac8436c32
NGUYEN_HOANG_ANH <nhanh100503@gmail.com>
2025-03-17 14:11:29 +0700

    Update VehicleType

---
+++ src/main/java/com/gis/mapper/VehicleTypeMapper.java
@@ -0,0 +1,14 @@
+package com.gis.mapper;
+
+import com.gis.dto.vehicleType.VehicleTypeResponse;
+import com.gis.model.VehicleType;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface VehicleTypeMapper {
+    VehicleTypeResponse vehicleTypeResponse(VehicleType vehicleType);
+    List<VehicleTypeResponse> vehicleTypeResponses(List<VehicleType> vehicleTypes);
+}
